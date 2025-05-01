66a52036578f935f7dae89575c604cd6563b203b
cyyeh <d09942009@ntu.edu.tw>
2025-05-02 04:20:27 +0800

    just another commitment to greatness

--- app/baseline/similarity.py
+++ app/baseline/similarity.py
@@ -1,3 +1,4 @@
 def run(args, item_pool, requests):
     # args.app in ['sim_query_item', 'sim_llm_item', 'sim_cot_item']:
+    # direct similairty; llm(query) vs item; llm(cot(query)) vs item;
     pass
